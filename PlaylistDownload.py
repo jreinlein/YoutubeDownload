8d7da029bfb348de2c0464812b8c80849f07aafa
James Reinlein <jreinlein@users.noreply.github.com>
2015-07-21 10:50:18 -0400

    Changed input, added support for mixes and singles
    Now accepts URLs for single songs and mixes (different from user-created playlists), and changed the input (now from external url.txt file) :v:

--- PlaylistDownload.py
+++ PlaylistDownload.py
@@ -1,7 +1,7 @@
 # Author: James Reinlein (2015)
 import os
 import time
-import tkinter as tk
+# import tkinter as tk
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,11 +11,17 @@ from selenium.webdriver.support import expected_conditions as EC
 url = ""
 
 def main():
+    # get URL from file
+    global url
+    f = open('url.txt', 'r')
+    url = f.readline()
+    '''
     # get URL from clipboard then run
     global url
     r = tk.Tk()
     url = r.selection_get(selection="CLIPBOARD")
     r.destroy()
+    '''
     download(webdriver.Firefox())
     
     
@@ -26,53 +32,37 @@ def profileSetup(playlistName):
     profile.set_preference('browser.download.folderList', 2)
     profile.set_preference('browser.download.manager.showWhenStarting', False)
     profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'audio/mpeg')
-    profile.set_preference('browser.download.dir', os.getcwd() + '\\' + playlistName)
+    if playlistName is None: # single song (same directory)
+        profile.set_preference('browser.download.dir', os.getcwd())
+    else:
+        profile.set_preference('browser.download.dir', os.getcwd() + '\\' + playlistName)
 
     return profile
 
-def cleanString(string):
-    for ch in ['/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '.']:
-        if ch in string:
-            string = string.replace(ch, '-')
-    return string
-        
+
      
 def download(driver):
-    print('Getting video URLs...', end=" ")
+    print('Getting video URL(s)...', end=" ")
     downloadLink = 'http://www.youtube-mp3.org/'
     yt = 'https://www.youtube.com/watch?v='
 
     driver.get(url)
-    # navigate to get all URLs on one page
-    driver.find_element_by_class_name("playlist-title").click()
-    while True:
-        try:
-            button = WebDriverWait(driver, 3).until(
-                EC.presence_of_element_located((By.XPATH, "//button[contains(., 'Load more')]")))
-        except:
-            break # nothing left to load
-
-        button.click() # load more
-        time.sleep(1) # needed, or else it clicks too quickly and messes up
-
-    # get all URLs
-    playlist = driver.find_element_by_id("pl-video-table")
-    items = playlist.find_elements(By.TAG_NAME, value='tr')
     
-    urls = []
-    for item in items:
-        urls.append(item.get_attribute("data-video-id"))
+    category = getCategory(driver) # single, mix, or playlist
 
+    # create folder to hold songs (mixes and playlists only)
+    title = None
+    if category != "single":
+        title = driver.find_element_by_class_name("playlist-title").text
+        title = cleanString(title)
+        if not os.path.exists(os.getcwd() + '\\' + title): os.makedirs(title)
 
-    # get playlist title and remove forbidden chars
-    title = driver.find_element_by_class_name("pl-header-title").text
-    title = cleanString(title)
-    if not os.path.exists(os.getcwd() + '\\' + title): os.makedirs(title)
+    urls = getUrls(driver, category)
 
     # close driver and open up new one
-    print(len(urls), "videos found!")
+    print(len(urls), "video(s) found!")
     driver.close()
-    print('Starting downloads...', end="")
+    print('Starting download(s)...', end=" ")
 
     # set proper download settings
     profile = profileSetup(title)
@@ -91,14 +81,65 @@ def download(driver):
 
         dlLink = driver.find_element_by_link_text('Download')
         dlLink.click()
-        #break # used for testing purposes
-    print("Done! Please allow downloads to finish.")
-    
+        # break # used for testing purposes
+    print("Done!")
+    print("Please allow download(s) to finish before closing browser.")
 
-'''
-def checkURL(url):
-    if 'youtube.com' or '&list=' not in url[:]:
-        print('Failure')
-'''
+    
+def cleanString(string):
+    for ch in ['/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '.']:
+        if ch in string:
+            string = string.replace(ch, '-')
+    return string
+
+
+def getCategory(driver):
+    if len(driver.find_elements(By.CLASS_NAME, "playlist-info")) < 1:
+        return "single"
+    else:
+        title = driver.find_element_by_class_name("author-attribution").text
+        if title.strip().lower() == "by YouTube".lower():
+            return "mix"
+        else:
+            return "playlist"
+
+
+def getUrls(driver, category):
+    urls = []
+    
+    if category == "single":
+        url = driver.current_url
+        url = url[(url.find("v=") + 2):]
+        urls.append(url)
+        
+    elif category == "mix":
+        playlist = driver.find_element_by_id("playlist-autoscroll-list")
+
+        items = playlist.find_elements(By.TAG_NAME, value='li')
+        for item in items:
+            urls.append(item.get_attribute('data-video-id'))
+
+    else: # playlist
+        # navigate to get all URLs on one page
+        driver.find_element_by_class_name("playlist-title").click()
+        while True:
+            try:
+                button = WebDriverWait(driver, 3).until(
+                      EC.presence_of_element_located((By.XPATH, "//button[contains(., 'Load more')]")))
+            except:
+                break # nothing left to load
+
+            button.click() # load more
+            time.sleep(1) # needed, or else it clicks too quickly and messes up
+
+        # get all URLs
+        playlist = driver.find_element_by_id("pl-video-table")
+        items = playlist.find_elements(By.TAG_NAME, value='tr')
+    
+        for item in items:
+            urls.append(item.get_attribute("data-video-id"))
+
+    return urls
+        
 
 main()
